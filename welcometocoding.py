ecb79961487daa3f2f431693a8a93bb916420da8
THSayeb <desktop499@gmail.com>
2024-11-29 21:18:22 +0600

    1st_py

---
+++ welcometocoding.py
@@ -0,0 +1,6 @@
+print("Hello world")
+#start
+print(67)
+print("Well done \n")
+print("Hello", 3)
+print("pneumosaassadddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssno...", end="50%")
