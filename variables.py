f7baf80640f54f49b6a10485f0002618d7516f07
THSayeb <desktop499@gmail.com>
2024-12-04 18:09:28 +0600

    first py

---
+++ variables.py
@@ -0,0 +1,9 @@
+a = "hello"
+num1 = 97
+print(a, num1)
+word = "Kayak"
+print(word, " ", a)
+print("\n")
+#introduction
+name = input("Enter your name")
+print(a, name, "Welcome to codingal..", end="%")
