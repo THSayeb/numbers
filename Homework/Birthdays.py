9fd9bd6d632a40bae2eb897e41e631c2d196ad2a
THSayeb <desktop499@gmail.com>
2024-12-05 15:46:06 +0600

    numbers

---
+++ Homework/Birthdays.py
@@ -0,0 +1,19 @@
+print("__________________________________________________________________________________")
+print("Welcome the number center")
+print("Here you can give ten numbers and check that if any of them has matched with other")
+print("Now, Provide ten number")
+
+num1 = input("Provide a number")
+num2 = input("Provide another number")
+num3 = input("Provide another number")
+num4 = input("Provide another number")
+num5 = input("Provide another number")
+num6 = input("Provide another number")
+num7 = input("Provide another number")
+num8 = input("Provide another number")
+num9 = input("Provide another number")
+num0 = input("Provide another number")
+
+
+print("The numbers are: ")
+print(num1, "  ", num2, " ", num3, " ", num4, " ", num5, " ", num6, " ", num7, " ", num8, " ", num9, " ", num0)
